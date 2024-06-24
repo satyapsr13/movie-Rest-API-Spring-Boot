bd61dff927b65b89471775e027c019ebb221ca3c
satyapsr13 <satyapsr13@gmail.com>
2024-06-24 15:49:56 +0530

    project setup done

--- src/main/java/com/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/movieflix/repositories/MovieRepository.java
@@ -1,2 +1,9 @@
-package com.movieflix.repositories;public class MovieRepository {
+package com.movieflix.repositories;
+
+import com.movieflix.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie,Integer> {
+
+
 }
