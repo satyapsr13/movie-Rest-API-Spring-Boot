b4d171d2924178f97dff3a118c38b5c9b3b12e9a
satyapsr13 <satyapsr13@gmail.com>
2024-06-25 16:40:14 +0530

    working on movieService section

--- src/main/java/com/movieflix/service/MovieService.java
+++ src/main/java/com/movieflix/service/MovieService.java
@@ -1,2 +1,15 @@
-package com.movieflix.service;public class MovieService {
+package com.movieflix.service;
+
+import com.movieflix.dto.MovieDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface MovieService {
+
+    MovieDto addMovie(MovieDto movieDto, MultipartFile file);
+
+    MovieDto getMovie(Integer movieId);
+
+    List<MovieDto> getALlMovies();
 }
--- src/main/java/com/movieflix/service/MovieServiceImpl.java
+++ src/main/java/com/movieflix/service/MovieServiceImpl.java
@@ -1,2 +1,47 @@
-package com.movieflix.service;public class MovieServiceImpl {
+package com.movieflix.service;
+
+import com.movieflix.dto.MovieDto;
+import com.movieflix.repositories.MovieRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+public class MovieServiceImpl implements MovieService{
+
+    private  final MovieRepository movieRepository;
+
+    public MovieServiceImpl(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public MovieDto addMovie(MovieDto movieDto, MultipartFile file) {
+    // 1. upload the file
+
+    // 2. set value of fiel post as filename
+
+    // 3. map dto to movie object
+
+    // 4. save the movie to movie object -> save movie object
+
+    // 5. generate posterUrl
+
+    // 6. map Movie object to DTO object and return it
+
+
+
+        return null;
+    }
+
+    @Override
+    public MovieDto getMovie(Integer movieId) {
+        return null;
+    }
+
+    @Override
+    public List<MovieDto> getALlMovies() {
+        return null;
+    }
 }
